c83107fed01361700db778f0cb3fab390473b78b
Dylan Sotomayor <dysotoma@e1z3r6p9.42.us.org>
2018-03-01 19:45:55 -0800

    libft

--- libft.h
+++ libft.h
@@ -24,6 +24,7 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 char	*ft_strstr(const char *str, const char *to_find);
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
 void    *ft_memmove(void *dst, const void *src, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_isalnum(int c);
 int     ft_isalpha(int c);
 int		ft_isascii(int c);
