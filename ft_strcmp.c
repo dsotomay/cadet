a2efd9326c6de5057e8aa63401b469f7a7cad2e6
dsotomay <dylants365@gmail.com>
2018-03-02 01:03:44 -0800

    Update ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -21,20 +21,11 @@ int		ft_strcmp(const char *str1, const char *str2)
 	i = 0;
 	s1 = (unsigned char *)str1;
 	s2 = (unsigned char *)str2;
-	if (s1[i] == '\0' || s2[i] == '\0')
-		return (s1[i] - s2[i]);
 	while (s1[i] == s2[i])
 	{
-		if(s[i] == '\0')
+		if(s1[i] == '\0')
 			return 0;
 		i++;
 	}
 	return (s1[i] - s2[i]);
-// 	while (s1[i])
-// 	{
-// 		if (s1[i] != s2[i])
-// 			return (s1[i] - s2[i]);
-// 		i++;
-// 	}
-// 	return (0);
 }
