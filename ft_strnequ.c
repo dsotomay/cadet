176178d9f42438bd8feb7569115a1151ff59570c
Dylan Sotomayor <dysotoma@e1z3r6p9.42.us.org>
2018-03-07 01:25:37 -0800

    part 2 fix

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -17,12 +17,14 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while (s1[i] == s2[i] && i <= n)
+	while (s1[i] == s2[i] && i < n)
 	{
 		if (s1[i])
 			i++;
 		else
 			return (1);
 	}
+	if (s1[i] || i == n)
+		return (1);
 	return (0);
 }
